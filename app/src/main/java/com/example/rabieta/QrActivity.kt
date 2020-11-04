0fab5edfda80f65f2415f8a8911dd665fd01cd87
lisan <lisandro.rima@istea.com.ar>
2020-11-04 18:43:20 -0300

    QR reader

---
+++ app/src/main/java/com/example/rabieta/QrActivity.kt
@@ -0,0 +1,85 @@
+package com.example.rabieta
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
+import kotlinx.android.synthetic.main.activity_qrscanner.*
+//import java.util.jar.Manifest
+
+
+class QrActivity : AppCompatActivity() {
+    val CAMERA_PERM = 111
+    lateinit var codeScanner: CodeScanner
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qrscanner)
+        setupUI()
+
+    }
+
+    private fun setupUI() {
+        codeScanner = CodeScanner(this,scannerView)
+        codeScanner.camera = CodeScanner.CAMERA_BACK
+        codeScanner.formats = CodeScanner.ALL_FORMATS
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE
+        codeScanner.scanMode = ScanMode.SINGLE
+        codeScanner.isAutoFocusEnabled=true
+        codeScanner.isFlashEnabled=false
+        
+        codeScanner.decodeCallback = DecodeCallback{
+            runOnUiThread {
+                Toast.makeText(this, "Scan result: ${it.text} ", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        codeScanner.errorCallback = ErrorCallback {
+            runOnUiThread {
+                Toast.makeText(this, "Camera error: ${it.message}", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        checkPermission()
+    }
+
+    fun checkPermission(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA),CAMERA_PERM)
+        }else{
+            codeScanner.startPreview()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+
+        if (requestCode ==CAMERA_PERM&&grantResults.isNotEmpty()&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            codeScanner.startPreview()
+        }else{
+            Toast.makeText(this,"No se puede activar hasta dar permisos",Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+}
