b5274d629d82c1d6de2d0f7d9eaa81be503840ac
Facu <64644557+fsayago@users.noreply.github.com>
2020-11-14 00:25:29 -0300

    Creado AboutUs

--- app/src/main/java/com/example/rabieta/AboutUsActivity.kt
+++ app/src/main/java/com/example/rabieta/AboutUsActivity.kt
@@ -2,10 +2,34 @@ package com.example.rabieta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 
 class AboutUsActivity : AppCompatActivity() {
+
+    private lateinit var toolbarAUs : Toolbar
+    private lateinit var txtTitulo : TextView
+    private lateinit var txtDescripcionAbouUs : TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_us)
+        setupUI()
+    }
+
+    private fun setupUI() {
+        setupToolbar()
+        txtTitulo = findViewById(R.id.txtTituloAboutUs)
+        txtDescripcionAbouUs = findViewById(R.id.txtDescripcionAboutUs)
+
+        txtTitulo.text = getString(R.string.TituloAboutUs)
+        txtDescripcionAbouUs.text = getString(R.string.DescripcionAus)
+    }
+
+    private fun setupToolbar() {
+        toolbarAUs = findViewById(R.id.toolbarAboutUs)
+        setSupportActionBar(toolbarAUs)
+        supportActionBar?.title = getString(R.string.ToolbarAboutUs)
     }
 }
